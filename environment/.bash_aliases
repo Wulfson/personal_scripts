49a186a47e0d29ba53bc7f11490f9ba43faa607e
Josh Hancock <joshua.hancock@inmar.com>
2022-06-08 13:49:02 -0400

    Sublime & Linux env settings

--- environment/.bash_aliases
+++ environment/.bash_aliases
@@ -1,8 +1,18 @@
+export HISTSIZE=8000
+export HISTFILESIZE=-1
 export HISTIGNORE="&:ll:ls:[bf]g:exit"
 export TERM='xterm-256color'
 export EDITOR=vim
 
-alias gx='grep -nR --exclude-dir=".git" --exclude="*.pyc"'
-alias sfix="export SSH_AUTH_SOCK=\$(find /tmp/ssh-* -user $USER -name agent\* -printf '%T@ %p\n' 2>/dev/null | sort -k 1nr | sed 's/^[^ ]* //' | head -n 1)"
+export PATH=$HOME'/bin:'$PATH
+export PYTHONSTARTUP=~/python_interpreter_startup.py
+
 alias sresume='screen -t bash -h 5000 -d -R -S bash'
 alias srshort='screen -c /home/ubuntu/.screenrc_short -t bash -h 5000 -d -R -S bash'
+alias gx='grep -nR --exclude-dir=".git" --exclude="*.pyc"'
+alias sfix="export SSH_AUTH_SOCK=\$(find /tmp/ssh-* -user $USER -name agent\* -printf '%T@ %p\n' 2>/dev/null | sort -k 1nr | sed 's/^[^ ]* //' | head -n 1)"
+
+for file in ./scripts/*;
+ do
+      source $file;
+ done
---
+++ environment/python_interpreter_startup.py
@@ -0,0 +1,101 @@
+# Script for use with PYTHONSTARTUP env variable
+
+import importlib
+import json
+import readline
+import sys
+
+from contextlib import contextmanager
+from enum import Enum
+
+
+# Set interpreter prompts
+sys.ps1 = "\033[0;32m>> \033[0m"
+sys.ps2 = "\033[0;34m.. \033[0m"
+
+
+class Printer(Enum):
+    """
+    A simple class to print colorized CLI messages without adding any external dependencies.
+    Usage: Printer.{color}("Message")
+    Example: Printer.red("Everything is horrible! What have you done!?")
+    """
+    red = "\033[1;31m"
+    green = "\033[1;32m"
+    yellow = "\033[1;33m"
+    blue = "\033[1;34m"
+    magenta = "\033[1;35m"
+    cyan = "\033[1;36m"
+    default = "\033[0m"
+    reset = "\033[0m"
+
+    def __call__(self, *messages):
+        print(
+            "{color_sequence}{message}{reset_sequence}".format(
+                color_sequence=self.value,
+                message="\n".join(map(str, messages)),
+                reset_sequence=Printer.reset.value,
+            ),
+            file=sys.stderr,
+        )
+
+
+try:
+    import boto3
+except Exception:
+    Printer.yellow("boto3 is not available")
+
+try:
+    import psycopg2
+    from psycopg2.extras import RealDictCursor
+except Exception:
+    Printer.yellow("psycopg2 is not available")
+
+
+# Make it easier to copy & paste JSON blobs
+null = None
+true = True
+false = False
+
+
+def jdump(obj, print_out=True):
+    out = json.dumps(obj, indent=4, default=str, sort_keys=True)
+    if print_out:
+        Printer.blue(out)
+    else:
+        return out
+
+
+db_creds = {"host": "localhost", "dbname": "devops", "user": "devops-schema-user", "password": None, "port": 5432}
+
+
+@contextmanager
+def pg_connect(db_creds):
+    conn = psycopg2.connect(**db_creds)
+    yield conn.cursor(cursor_factory=RealDictCursor)
+    conn.close()
+
+
+def query(sql, data=None):
+    res = None
+    with pg_connect(db_creds) as curs:
+        curs.execute(sql, data)
+        return curs.fetchall()
+
+
+def reload_module(name):
+    importlib.reload(sys.modules[name])
+    Printer.magenta("Reloaded " + name)
+
+
+def history(max_lines=50):
+    history_length = readline.get_current_history_length()
+
+    start_line = 1
+    if max_lines > 0 and history_length > max_lines:
+        start_line = history_length - max_lines
+
+    Printer.blue(*[str(readline.get_history_item(i + start_line)) for i in range(history_length - start_line)])
+
+
+Printer.cyan("Interpreter tools configured")
